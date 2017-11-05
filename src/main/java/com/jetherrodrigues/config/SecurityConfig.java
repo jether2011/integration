853337690f32767f37b3fec70a290f3de557e789
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2017-11-05 00:50:44 -0200

    Add security config to allow the api url to be consumed

---
+++ src/main/java/com/jetherrodrigues/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.jetherrodrigues.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("test@gmail.com")
+			.password("123")
+			.roles("ADMIN");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("favicon.png");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers()
+				.hasRole("ADMIN")
+				.antMatchers("/api/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.and().httpBasic()
+				.and().csrf().disable();
+
+	}
+}
